05e15ba5369aa5495ef13de9dab2ce0633980750
Ignacio Vargas <ignavaoc17@gmail.com>
2018-06-16 20:49:45 -0600

    version 1 de Override

--- src/main/java/com/simulador/modules/QueryExecutionerModule.java
+++ src/main/java/com/simulador/modules/QueryExecutionerModule.java
@@ -1,7 +1,14 @@
 package com.simulador.modules;
 
+import com.simulador.dbms.Event;
+
 /**
  *
  */
 public class QueryExecutionerModule extends GeneralModule{
+
+    @Override
+    void processEvent(Event event) {
+        super.processEvent(event);
+    }
 }
